3be710763355e4bbbd8bd195c474dbb92a110c6f
Bill Peterson <albedozero@gmail.com>
2022-01-28 10:12:41 -0600

    bump version

--- patcher/__init__.py
+++ patcher/__init__.py
@@ -9,7 +9,7 @@ except ModuleNotFoundError:
     pass # mido is optional, only needed to read .syx and send to external devices
 from . import fswrap, fpyaml
 
-VERSION = '0.5.11'
+VERSION = '0.6'
 
 CC_DEFAULTS = [(7, 7, 100),    # volume
                (10, 10, 64),   # pan
