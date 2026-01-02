7f59ef9f81e35ac6feb2033596c7a2fd5d43ce1c
albedozero <albedozero@gmail.com>
2026-01-02 15:33:25 -0600

    update examples

--- src/fluidpatcher/examples/basic.py
+++ src/fluidpatcher/examples/basic.py
@@ -19,24 +19,24 @@ print(f"Loaded bank: {bank_file.name}")
 print("Patches:")
 for i, name in enumerate(fp.bank.patches, start=1):
     print(f"{i:>5}) {name}")
-
+    
 # select the first patch
 patch_index = 0
 fp.apply_patch(fp.bank.patches[patch_index])
 
 # main loop
 while True:
-    cmd = input("Enter = next patch | q = quit: ").strip().lower()
+    print(
+        f"Applied patch: {fp.bank.patches[patch_index]} "
+        f"({patch_index + 1}/{len(fp.bank.patches)})"
+    )
+
+    cmd = input("Enter = next patch | q = quit: ").lower()
 
     if cmd == "q":
         break
-
-    elif cmd == "":
+    else:
         patch_index = (patch_index + 1) % len(fp.bank.patches)
         patch_name = fp.bank.patches[patch_index]
         fp.apply_patch(patch_name)
-        print(
-            f"Patch: {patch_name} "
-            f"({patch_index + 1}/{len(fp.bank.patches)})"
-        )
 
